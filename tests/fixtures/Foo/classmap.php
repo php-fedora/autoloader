56da9c8b3bba516089edea511c421f4179a6ee5c
Shawn Iwinski <siwinski@redhat.com>
2016-10-25 11:43:24 -0400

    [res/phpab/fedora.php.tpl] Stylish updates
    - Run php-cs-fixer
    - Add ignore coding standards annotation
    - Add ignore code coverage annotation
    - Re-run tests/genclassmap.sh

--- tests/fixtures/Foo/classmap.php
+++ tests/fixtures/Foo/classmap.php
@@ -1,11 +1,13 @@
 <?php
-
-require_once __DIR__.'/../../../src' . '/autoload.php';
+// @codingStandardsIgnoreFile
+// @codeCoverageIgnoreStart
+require_once __DIR__.'/../../../src'.'/autoload.php';
 
 \Fedora\Autoloader\Autoload::addClassMap(
     array(
         'foo\\bar' => '/Bar.php',
-                'foo\\bar\\baz' => '/Bar/Baz.php'
+                'foo\\bar\\baz' => '/Bar/Baz.php',
     ),
     __DIR__
 );
+// @codeCoverageIgnoreEnd
--- tests/fixtures/Foo2/classmap.php
+++ tests/fixtures/Foo2/classmap.php
@@ -1,10 +1,12 @@
 <?php
-
-require_once __DIR__.'/../../../src' . '/autoload.php';
+// @codingStandardsIgnoreFile
+// @codeCoverageIgnoreStart
+require_once __DIR__.'/../../../src'.'/autoload.php';
 
 \Fedora\Autoloader\Autoload::addClassMap(
     array(
-        'foo\\bar' => '/Bar.php'
+        'foo\\bar' => '/Bar.php',
     ),
     __DIR__
 );
+// @codeCoverageIgnoreEnd
